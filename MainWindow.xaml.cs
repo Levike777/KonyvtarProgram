018582b99613768b7da93d53cd1e8742b39d31bf
Petrik Diak <diak@petrik.hu>
2025-11-25 13:37:26 +0100

    3.feladat

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,65 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace KonyvtarProgram
+namespace Konyvtar
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainWindow : Window
+	{
+		string fajlNev = "olvasok.txt";
+		List<Olvaso> olvasok = new List<Olvaso>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			BetoltOlvasok();
+		}
+
+		private void BetoltOlvasok()
+		{
+			if (File.Exists(fajlNev))
+			{
+				foreach (var sor in File.ReadAllLines(fajlNev))
+				{
+					var o = Olvaso.FromString(sor);
+					olvasok.Add(o);
+					lbOlvasok.Items.Add(o.Nev);
+				}
+			}
+		}
+
+		private void btnMent_Click(object sender, RoutedEventArgs e)
+		{
+			if (!int.TryParse(tbEletkor.Text, out int eletkor))
+				return;
+
+			string tagsag =
+				rbNormal.IsChecked == true ? "Normál" :
+				rbDiak.IsChecked == true ? "Diák" : "Nyugdíjas";
+
+			var o = new Olvaso
+			{
+				Nev = tbNev.Text,
+				Eletkor = eletkor,
+				Mufaj = ((ComboBoxItem)cbMufaj.SelectedItem)?.Content.ToString() ?? "",
+				Hirlevel = cbHirlevel.IsChecked == true,
+				SmsErtesites = cbSms.IsChecked == true,
+				Tagsag = tagsag
+			};
+
+			File.AppendAllText(fajlNev, o + Environment.NewLine);
+			olvasok.Add(o);
+			lbOlvasok.Items.Add(o.Nev);
+			tbUzenet.Text = "Regisztráció sikeres.";
+
+			tbNev.Clear();
+			tbEletkor.Clear();
+			cbMufaj.SelectedIndex = -1;
+			cbHirlevel.IsChecked = false;
+			cbSms.IsChecked = false;
+			rbNormal.IsChecked = true;
+		}
+	}
 }
--- Olvaso.cs
+++ Olvaso.cs
@@ -1,28 +1,33 @@
-﻿public class Olvaso
+﻿using System;
+
+namespace Konyvtar
 {
-	public string Nev { get; set; }
-	public int Eletkor { get; set; }
-	public string Mufaj { get; set; }
-	public bool Hirlevel { get; set; }
-	public bool SmsErtesites { get; set; }
-	public string Tagsag { get; set; }
-
-	public override string ToString()
+	public class Olvaso
 	{
-		return $"{Nev}|{Eletkor}|{Mufaj}|{Hirlevel}|{SmsErtesites}|{Tagsag}";
-	}
+		public string Nev { get; set; }
+		public int Eletkor { get; set; }
+		public string Mufaj { get; set; }
+		public bool Hirlevel { get; set; }
+		public bool SmsErtesites { get; set; }
+		public string Tagsag { get; set; }
 
-	public static Olvaso FromString(string sor)
-	{
-		var parts = sor.Split('|');
-		return new Olvaso
+		public override string ToString()
 		{
-			Nev = parts[0],
-			Eletkor = int.Parse(parts[1]),
-			Mufaj = parts[2],
-			Hirlevel = bool.Parse(parts[3]),
-			SmsErtesites = bool.Parse(parts[4]),
-			Tagsag = parts[5]
-		};
+			return $"{Nev}|{Eletkor}|{Mufaj}|{Hirlevel}|{SmsErtesites}|{Tagsag}";
+		}
+
+		public static Olvaso FromString(string sor)
+		{
+			var p = sor.Split('|');
+			return new Olvaso
+			{
+				Nev = p[0],
+				Eletkor = int.Parse(p[1]),
+				Mufaj = p[2],
+				Hirlevel = bool.Parse(p[3]),
+				SmsErtesites = bool.Parse(p[4]),
+				Tagsag = p[5]
+			};
+		}
 	}
 }
