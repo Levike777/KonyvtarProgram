15a5eb089906a5acfd2eb8b0330c5db0ff56ebc5
Petrik Diak <diak@petrik.hu>
2025-11-25 13:29:51 +0100

    Add project files.

---
+++ Olvaso.cs
@@ -0,0 +1,28 @@
+﻿public class Olvaso
+{
+	public string Nev { get; set; }
+	public int Eletkor { get; set; }
+	public string Mufaj { get; set; }
+	public bool Hirlevel { get; set; }
+	public bool SmsErtesites { get; set; }
+	public string Tagsag { get; set; }
+
+	public override string ToString()
+	{
+		return $"{Nev}|{Eletkor}|{Mufaj}|{Hirlevel}|{SmsErtesites}|{Tagsag}";
+	}
+
+	public static Olvaso FromString(string sor)
+	{
+		var parts = sor.Split('|');
+		return new Olvaso
+		{
+			Nev = parts[0],
+			Eletkor = int.Parse(parts[1]),
+			Mufaj = parts[2],
+			Hirlevel = bool.Parse(parts[3]),
+			SmsErtesites = bool.Parse(parts[4]),
+			Tagsag = parts[5]
+		};
+	}
+}
